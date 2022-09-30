7f4c299e715e47eff7aa25712ec1fb4f3081bc75
Jeremiah Payne <jeremiahp@redmondinc.com>
2022-09-30 15:21:27 -0600

    added a newline char.

--- internal/handler/commands/say.go
+++ internal/handler/commands/say.go
@@ -7,7 +7,7 @@ import (
 func (h BotCommandHelp) Say(request BotCommand) (response HelpResponse) {
     response.Help = "Give Bender a line of text to say in a channel."
 
-    response.Description = "Cause the bot to say something in a channel. " +
+    response.Description = "Cause the bot to say something in a channel. \n" +
     "Usage: '!say in {channel} {text}' or '!say {text}' for same channel."
 
     return response
