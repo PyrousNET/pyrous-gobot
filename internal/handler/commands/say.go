9a77bc45c760b0fd997ed588e60b931c3dae8ecc
Jeremiah Payne <jeremiahp@redmondinc.com>
2022-09-30 14:32:38 -0600

    Added more detail to the help

--- internal/handler/commands/say.go
+++ internal/handler/commands/say.go
@@ -7,7 +7,8 @@ import (
 func (h BotCommandHelp) Say(request BotCommand) (response HelpResponse) {
     response.Help = "Give Bender a line of text to say in a channel."
 
-    response.Description = "Cause the bot to say something in a channel. Usage: '!say in {channel} {text}'"
+    response.Description = "Cause the bot to say something in a channel. " +
+    "Usage: '!say in {channel} {text}' or '!say {text}' for same channel."
 
     return response
 }
