89547fff4e8c40765e3c7f8047666d3e16503eaa
Benjamin Payne <trixtur@gmail.com>
2022-10-01 17:40:41 -0600

    Stubbed out farkle game

---
+++ internal/handler/games/farkle.go
@@ -0,0 +1,12 @@
+package games
+
+import "fmt"
+
+func (bg BotGame) Farkle(event BotGame) (response Response, err error) {
+	response.Type = "command"
+	response.Message = fmt.Sprintf("/echo %s would like to play a game of Farkle.", event.sender)
+
+	// TODO - for kr0w?
+
+	return response, nil
+}
