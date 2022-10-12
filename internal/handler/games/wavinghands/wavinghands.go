4b7dd12cc0cbccdcab267c2dfdfa61db1a730f94
Benjamin Payne <trixtur@gmail.com>
2022-10-11 21:02:41 -0600

    Refactored code to flat file-system for now.

--- internal/handler/games/wavinghands/wavinghands.go
+++ internal/handler/games/wavinghands/wavinghands.go
@@ -1,12 +1,37 @@
 package wavinghands
 
+const (
+	minTeams = 2
+	maxTeams = 6
+)
+
 type (
-	Gesture struct {
-		Name  string
-		Value uint
+	Spell struct {
+		Name        string `json:"name"`
+		Sequence    string `json:"sequence"`
+		ShSequence  string `json:"sh-sequence"`
+		Description string `json:"description"`
+		Usage       string `json:"usage"`
+		Damage      int    `json:"damage"`
+		Resistances string `json:"resistances"`
+		Protections string `json:"protections"`
+	}
+	Monster struct {
+		Type        string `json:"type"`
+		HitPoints   int    `json:"hp"`
+		Curses      string `json:"curses"`
+		Protections string `json:"protections"`
 	}
 
-	Spell struct {
-		Gestures []Gesture
+	Wizard struct {
+		WhPlaying   string  `json:"wh-playing"`
+		Name        string  `json:"name"`
+		HitPoints   int     `json:"hp"`
+		Curses      string  `json:"curses"`
+		Protections string  `json:"protections"`
+		Monsters    Monster `json:"monsters"`
+	}
+
+	GameData struct {
 	}
 )
