3a345f2c42b39961b25b75d8c60b893ab95578ca
Kevin Payne <payneks@gmail.com>
2022-11-05 05:21:05 -0600

    emote replies through goroutine

--- internal/handler/commands/emote.go
+++ internal/handler/commands/emote.go
@@ -2,11 +2,22 @@ package commands
 
 import (
 	"fmt"
+	"strings"
+
+	"github.com/pyrousnet/pyrous-gobot/internal/comms"
+	"github.com/pyrousnet/pyrous-gobot/internal/users"
 )
 
-func (bc BotCommand) Emote(event BotCommand) (response Response, err error) {
-	response.Type = "command"
+func (bc BotCommand) Emote(event BotCommand) error {
+	u, _, _ := users.GetUser(strings.TrimLeft(event.sender, "@"), event.cache)
+	response := comms.Response{
+		ReplyChannelId: event.ReplyChannel.Id,
+		UserId:         u.Id,
+		Type:           "command",
+	}
+
 	response.Message = fmt.Sprintf(`/me %s`, event.body)
 
-	return response, nil
+	event.ResponseChannel <- response
+	return nil
 }
