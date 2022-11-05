9261a156b8d6adccb7cd5e95f91302f0fc28e557
Kevin Payne <payneks@gmail.com>
2022-11-05 05:28:30 -0600

    message replies through goroutine

--- internal/handler/commands/message.go
+++ internal/handler/commands/message.go
@@ -3,16 +3,25 @@ package commands
 import (
 	"fmt"
 	"strings"
+
+	"github.com/pyrousnet/pyrous-gobot/internal/comms"
+	"github.com/pyrousnet/pyrous-gobot/internal/users"
 )
 
-func (bc BotCommand) Message(event BotCommand) (response Response, err error) {
-	response.Type = "command"
+func (bc BotCommand) Message(event BotCommand) error {
+	u, _, _ := users.GetUser(strings.TrimLeft(event.sender, "@"), event.cache)
+	response := comms.Response{
+		ReplyChannelId: event.ReplyChannel.Id,
+		UserId:         u.Id,
+		Type:           "command",
+	}
 
 	channelObj, _ := event.mm.GetChannel(event.mm.DebuggingChannel.Name)
-	response.Channel = channelObj.Id
+	response.ReplyChannelId = channelObj.Id
 
 	target := strings.TrimLeft(event.target, "@")
 	response.Message = fmt.Sprintf(`/msg %s %s`, target, event.body)
 
-	return response, nil
+	event.ResponseChannel <- response
+	return nil
 }
