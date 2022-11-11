1fbbab8efe7e2c87f74f74dc13fb0e683a98a190
Benjamin Payne <trixtur@gmail.com>
2022-11-11 09:55:31 -0700

    Fixes waving hands tests. The issue was that the test needed to be run in the go routine and the channel needed to be handled in the main rountine's test.

--- internal/handler/games/wh_test.go
+++ internal/handler/games/wh_test.go
@@ -1,7 +1,6 @@
 package games
 
 import (
-	"fmt"
 	"github.com/mattermost/mattermost-server/v6/model"
 	"github.com/pyrousnet/pyrous-gobot/internal/cache"
 	"github.com/pyrousnet/pyrous-gobot/internal/comms"
@@ -20,6 +19,7 @@ func Test_handleEmptyBody(t *testing.T) {
 		args  args
 		want  error
 		want1 bool
+		want2 comms.Response
 	}{
 		{
 			name: "empty input",
@@ -38,9 +38,16 @@ func Test_handleEmptyBody(t *testing.T) {
 			},
 			want:  nil,
 			want1: true,
+			want2: comms.Response{
+				ReplyChannelId: "test",
+				Message:        "player is missing a name",
+				Type:           "dm",
+				UserId:         "",
+				Quit:           nil,
+			},
 		},
 		{
-			name: "",
+			name: "test body",
 			args: args{
 				event: BotGame{
 					body:            "",
@@ -56,20 +63,30 @@ func Test_handleEmptyBody(t *testing.T) {
 			},
 			want:  nil,
 			want1: false,
+			want2: comms.Response{
+				ReplyChannelId: "test",
+				Message:        "/echo test would like to play a game of Waving Hands.\n",
+				Type:           "command",
+				UserId:         "",
+				Quit:           nil,
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			go func() {
-				got2 := <-rs
-				fmt.Println(got2)
+				got, got1 := handleEmptyBody(tt.args.event)
+				if got != tt.want {
+					t.Errorf("handleEmptyBody() got = %v, want %v", got1, tt.want1)
+				}
+
+				if got1 != tt.want1 {
+					t.Errorf("handleEmptyBody() got1 = %v, want1 %v", got1, tt.want1)
+				}
 			}()
-			got, got1 := handleEmptyBody(tt.args.event)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("handleEmptyBody() got = %v, want %v", got, tt.want)
-			}
-			if got1 != tt.want1 {
-				t.Errorf("handleEmptyBody() got2 = %v, want %v", got1, tt.want1)
+			got2 := <-rs
+			if !reflect.DeepEqual(got2, tt.want2) {
+				t.Errorf("handleEmptyBody() got2 = %v, want2 %v", got2, tt.want2)
 			}
 		})
 	}
